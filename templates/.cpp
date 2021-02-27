9f7a18cb1ebd17415573e498fe299997e0190a1b
Paulo <paulorccaraujo@gmail.com>
2021-02-27 16:08:16 +0100

    Update cpp template

--- templates/.cpp
+++ templates/.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+typedef long long ll;
+typedef vector<int> vi;
+
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     return 0;
 }
